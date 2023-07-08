2ce637727706ea09c5e5b46361076690667c1c62
Shubert <103430520+Sk0voroda@users.noreply.github.com>
2023-07-08 18:11:18 +0300

    Added Day 1 solution using Itertools

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 mod solutions_2022;
+use solutions_2022::{day01, day02};
 
 fn main() {
-    // println!(
-    //     "Max calories: {:?}",
-    //     solutions_2022::day01::top_three_calories()
-    // );
+    println!("Day 1 - {} result", day01::top_three_calories_itertools());
+
     println!(
-        "Rock Paper Scissors result: {}",
-        solutions_2022::day02::rock_paper_scissors(solutions_2022::day02::game_result_p_2)
+        "Day 2 - {} result",
+        day02::rock_paper_scissors(day02::game_result_p_2)
     );
 }
--- src/solutions_2022/day01.rs
+++ src/solutions_2022/day01.rs
@@ -1,5 +1,7 @@
 // https://adventofcode.com/2022/day/1
 
+use itertools::Itertools;
+
 // part one
 pub fn top_calories() -> usize {
     let file_data =
@@ -35,4 +37,24 @@ pub fn top_three_calories() -> usize {
     top_three.reverse();
 
     top_three.iter().take(3).copied().sum()
+}
+
+// part two using Itertools
+pub fn top_three_calories_itertools() -> usize {
+    let file_data =
+        std::fs::read_to_string("inputs/2022/01.txt").expect("puzzle day 1 file is missing");
+
+    let top_three_sum = file_data
+        .split("\n\n")
+        .map(|item| {
+            item.lines()
+                .map(|cal| cal.parse().unwrap_or(0))
+                .sum::<usize>()
+        })
+        .sorted()
+        .rev()
+        .take(3)
+        .sum();
+
+    top_three_sum
 }
